8a13ce38c70b8be2baf0c5bd8ed9d445c2de7746
twopi <ajharris@iupui.edu>
2018-11-24 12:20:36 -0500

    added tasklist array and update table

---
+++ HeatherHit.js
@@ -0,0 +1,34 @@
+var tl;
+
+function init(){
+	//pull tl from local storage
+	// can we put an array in local storage?
+	tl = ["evil plan", "world domination", "make cookies"]; 
+	
+} // end function
+
+function loadView(){
+  // customize the loadView page based on current task list
+	
+	document.location.href="#viewTasks";
+  // clear existing rows
+	$("#tblTasks tbody tr").remove();
+
+	// add data from tl
+  for (i = 0; i < tl.length; i++){
+		tblString = "<tr><td>" + tl[i] + "</td>\n";
+		tblString += "<td><button type = 'button'>\n";
+    tblString += "edit\n"
+	  tblString += "</button></td>\n";
+
+		tblString += "<td><button type = 'button'>\n";
+    tblString += "delete\n"
+	  tblString += "</button></td></tr>\n";
+
+		$("#tblTasks tbody").append(tblString);
+  } // end for loop
+				
+		//$("#tblTasks tbody").append("<tr><td>new thing</td><td></td></td></td></tr>");
+} // end loadView
+
+
